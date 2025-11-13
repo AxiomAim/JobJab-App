cc6e14678599da2f068c23dba9b2ff151311c820
Thomaas Powell <tom@axiomaim.com>
2025-11-13 08:03:06 -0500

    update

---
+++ src/app/modules/landing/customer-view/customer-layout/customer-layout.component.ts
@@ -0,0 +1,137 @@
+import { Component, inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { AxiomaimFullscreenComponent } from '@axiomaim/components/fullscreen';
+import { AxiomaimLoadingBarComponent } from '@axiomaim/components/loading-bar';
+import {
+    AxiomaimNavigationService,
+    AxiomaimVerticalNavigationComponent,
+} from '@axiomaim/components/navigation';
+import { AxiomaimMediaWatcherService } from '@axiomaim/services/media-watcher';
+import { NavigationService } from 'app/core/navigation/navigation.service';
+import { Navigation } from 'app/core/navigation/navigation.types';
+// import { User } from 'app/core/user/user.types';
+import { LanguagesComponent } from 'app/layout/common/languages/languages.component';
+import { MessagesComponent } from 'app/layout/common/messages/messages.component';
+import { NotificationsComponent } from 'app/layout/common/notifications/notifications.component';
+import { QuickChatComponent } from 'app/layout/common/quick-chat/quick-chat.component';
+import { SearchComponent } from 'app/layout/common/search/search.component';
+import { ShortcutsComponent } from 'app/layout/common/shortcuts/shortcuts.component';
+import { LoginUserMenuComponent } from 'app/layout/common/login-user-menu/login-user-menu.component';
+import { Subject, takeUntil } from 'rxjs';
+import { User } from 'app/modules/axiomaim/administration/users/users.model';
+import { LoginUserService } from 'app/core/login-user/login-user.service';
+import { FirebaseAuthV2Service } from 'app/core/auth-firebase/firebase-auth-v2.service';
+
+@Component({
+    selector: 'customer-layout',
+    templateUrl: './customer-layout.component.html',
+    encapsulation: ViewEncapsulation.None,
+    imports: [
+        AxiomaimLoadingBarComponent,
+        AxiomaimVerticalNavigationComponent,
+        LoginUserMenuComponent,
+        MatButtonModule,
+        MatIconModule,
+        LanguagesComponent,
+        AxiomaimFullscreenComponent,
+        SearchComponent,
+        ShortcutsComponent,
+        MessagesComponent,
+        NotificationsComponent,
+        RouterOutlet,
+        QuickChatComponent,
+    ],
+})
+export class CustomerLayoutComponent implements OnInit, OnDestroy {
+    loginUser = inject(FirebaseAuthV2Service).loginUser();
+    isScreenSmall: boolean;
+    navigation: Navigation;
+    // user: User;
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router,
+        private _navigationService: NavigationService,
+        private _axiomaimMediaWatcherService: AxiomaimMediaWatcherService,
+        private _axiomaimNavigationService: AxiomaimNavigationService
+    ) {}
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Accessors
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Getter for current year
+     */
+    get currentYear(): number {
+        return new Date().getFullYear();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        // Subscribe to navigation data
+        this._navigationService.navigation$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((navigation: Navigation) => {
+                this.navigation = navigation;
+            });
+
+        // Subscribe to the user service
+        // this._loginUserService.loginUser$
+        //     .pipe(takeUntil(this._unsubscribeAll))
+        //     .subscribe((user: User) => {
+        //         this.user = user;
+        //     });
+
+        // Subscribe to media changes
+        this._axiomaimMediaWatcherService.onMediaChange$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(({ matchingAliases }) => {
+                // Check if the screen is small
+                this.isScreenSmall = !matchingAliases.includes('md');
+            });
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Toggle navigation
+     *
+     * @param name
+     */
+    toggleNavigation(name: string): void {
+        // Get the navigation
+        const navigation =
+            this._axiomaimNavigationService.getComponent<AxiomaimVerticalNavigationComponent>(
+                name
+            );
+
+        if (navigation) {
+            // Toggle the opened status
+            navigation.toggle();
+        }
+    }
+}
